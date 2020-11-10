eb1322ccfce81fbb351754079722499763bd69f3
victorisimoo <vmelendrez2013241@gmail.com>
2020-11-10 07:59:15 -0600

    message module

--- src/com/application/controllers/ProfileSearchController.java
+++ src/com/application/controllers/ProfileSearchController.java
@@ -11,8 +11,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
