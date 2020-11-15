a00e8f9f5f5fab9976f3318c1b055a461d26f843
unknown <josedanielgrn01@gmail.com>
2020-11-15 13:42:51 -0600

    subida metodo imagenes

--- src/com/application/controllers/ProfileSearchController.java
+++ src/com/application/controllers/ProfileSearchController.java
@@ -5,16 +5,21 @@ import com.application.system.Principal;
 import com.application.system.Storage;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * @author victorisimo
@@ -24,8 +29,11 @@ public class ProfileSearchController implements Initializable {
     @FXML private Label lblUser;
     @FXML private Label lblName;
     @FXML private Label lblLastName;
-    @FXML private Label lblDate;  
+    @FXML private Label lblDate;
+    @FXML private ImageView pbxImage;
     @FXML private TextArea txtPublicMessages;
+    ArrayList<String> pictureList;
+    int counterPicture = 0;
     ArrayList<MessageBean> publicMessages = new ArrayList<>();
 
     @Override
@@ -42,6 +50,16 @@ public class ProfileSearchController implements Initializable {
         lblLastName.setText(Storage.Instance().secondUser.getLastName());
         lblDate.setText(Storage.Instance().secondUser.getBirth());
         txtPublicMessages.setDisable(true);
+        
+        try {
+            pictureList = findUserPictures(lblUser.getText());
+            if (pictureList != null) {
+                Image image1 = new Image(new FileInputStream(pictureList.toArray()[0].toString()));
+                pbxImage.setImage(image1);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ProfileSearchController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
     public void setTextAreaMessages(){
@@ -62,6 +80,44 @@ public class ProfileSearchController implements Initializable {
     
     public void returnProfile(){
         escenarioPrincipal.ventanaNormalUser();
+    }
+    
+    public ArrayList<String> findUserPictures(String user) throws IOException{
+        File file = new File("C:\\MEIA\\imagen_usuario.txt");
+        
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        
+        ArrayList<String> valueList = new ArrayList<>();
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line = bufferedReader.readLine();
+        
+        while (line != null) {
+            String[] arrayTemp = line.split("[|]");
+            if (arrayTemp[3].equals(user)) {
+                valueList.add(arrayTemp[4]);
+            }
+            line = bufferedReader.readLine();
+        }
+        return valueList;
+    }
+    
+     public void nextPicture() throws FileNotFoundException{
+        if (counterPicture+1 <= pictureList.toArray().length && pictureList != null) {
+            ++counterPicture;
+            Image image1 = new Image(new FileInputStream(pictureList.toArray()[counterPicture].toString()));
+            pbxImage.setImage(image1);
+        }
+    }
+     
+    public void previousPicture() throws FileNotFoundException{
+        if (counterPicture-1 >= 0 && pictureList != null) {
+            --counterPicture;
+            Image image1 = new Image(new FileInputStream(pictureList.toArray()[counterPicture].toString()));
+            pbxImage.setImage(image1);
+        }
     }
     
     public void obtenerMensajesPublicos(String username) throws FileNotFoundException, IOException{
